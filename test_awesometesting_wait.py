49ef00161d7722433cea7508259f73cea7602d07
DorDob <64862484+DorDob@users.noreply.github.com>
2020-05-10 14:02:03 +0200

    Update test_awesometesting_wait.py

--- test_awesometesting_wait.py
+++ test_awesometesting_wait.py
@@ -8,69 +8,66 @@ from selenium.webdriver.support import expected_conditions
 
 
 def test_post_count():
-    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
-    # ustawiana automatycznie przez bibliotekę webdriver-manager
+                                    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
+                                    # ustawiana automatycznie przez bibliotekę webdriver-manager
     browser = Chrome(executable_path=ChromeDriverManager().install())
 
-    # Otwarcie strony
+                                    # Otwarcie strony
     browser.get('https://www.awesome-testing.com/')
-    # Pobranie listy tytułów
+                                    # Pobranie listy tytułów
     list_of_titles = browser.find_elements(By.CSS_SELECTOR,'.post-title.entry-title')
 
-    # Asercja że lista ma 4 elementy
-    #assert len(list_of_titles) == 4
-
-    # Zamknięcie przeglądarki
+                                    # Asercja że lista ma 4 elementy                              
+    assert len(list_of_post) == 4
+                                    # Zamknięcie przeglądarki
 
 
 def test_post_count_after_search():
-    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
-    # ustawiana automatycznie przez bibliotekę webdriver-manager
+                                    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
+                                    # ustawiana automatycznie przez bibliotekę webdriver-manager
     browser = Chrome(executable_path=ChromeDriverManager().install())
 
-    # Otwarcie strony
+                                    # Otwarcie strony
     browser.get('https://www.awesome-testing.com/')
-    # Inicjalizacja searchbara i przycisku search
+                                    # Inicjalizacja searchbara i przycisku search
     search_bar = browser.find_element(By.CSS_SELECTOR,'input.gsc-input')
     search_button = browser.find_element(By.CSS_SELECTOR, 'input.gsc-search-button')
-    # Szukanie
+                                    # Szukanie
     search_bar.send_keys('cypress')
     search_button.click()
-    # Czekanie na stronę
-    #time.sleep(5)
+                                    # Czekanie na stronę
     wait = WebDriverWait(browser, 10)
     grey_status_bar = (By.CLASS_NAME, 'status-msg-body')
     wait.until(expected_conditions.visibility_of_element_located(grey_status_bar))
-    # Pobranie listy tytułów
+                                    # Pobranie listy tytułów
     titles = browser.find_elements(By.CLASS_NAME, 'post-title')
-    # Asercja że lista ma 3 elementy
+                                    # Asercja że lista ma 3 elementy
     assert len(titles) == 3
-    # Zamknięcie przeglądarki
+                                    # Zamknięcie przeglądarki
     browser.close()
 
 
 def test_post_count_on_cypress_label():
-    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
-    # ustawiana automatycznie przez bibliotekę webdriver-manager
+                                    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
+                                    # ustawiana automatycznie przez bibliotekę webdriver-manager
     browser = Chrome(executable_path=ChromeDriverManager().install())
 
-    # Otwarcie strony
+                                    # Otwarcie strony
     browser.get('https://www.awesome-testing.com/')
-    # Inicjalizacja elementu z labelką
+                                    # Inicjalizacja elementu z labelką
     label = browser.find_element(By.LINK_TEXT, 'Cypress')
-    # Kliknięcie na labelkę
+                                    # Kliknięcie na labelkę
 
     label.click()
-    # Czekanie na stronę
-    #time.sleep(5)
+                                    #Czekanie na stronę
     wait = WebDriverWait(browser, 10)
     grey_status_bar = (By.CLASS_NAME, 'status-msg-body')
     wait.until(expected_conditions.visibility_of_element_located(grey_status_bar))
 
-    # Pobranie listy tytułów
+                                    # Pobranie listy tytułów
     titles = browser.find_elements(By.CSS_SELECTOR, 'input.gsc-input')
-    # Asercja że lista ma 1 element
+                                    # Asercja że lista ma 1 element
     assert len(titles) == 1
-    # Zamknięcie przeglądarki
+                                    # Zamknięcie przeglądarki
 
     browser.close()
