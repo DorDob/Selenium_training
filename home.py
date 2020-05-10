e164c95adc902085696a80eddf5d7ac8e2101247
DorDob <64862484+DorDob@users.noreply.github.com>
2020-05-10 12:53:28 +0200

    Update home.py

--- home.py
+++ home.py
@@ -15,7 +15,7 @@ class HomePage:
     def verify_post_count(self, expected_count):
         list_of_post_titles = self.browser.find_elements(By.CSS_SELECTOR,'.post-title.entry-title')
 
-        assert len(list_of_post_titles) == expected_count     # Asercja że lista ma 4 elementy
+        assert len(list_of_post_titles) == expected_count     # Asercja - lista ma 4 elementy
 
     def label_click(self, label_name):
         label = self.browser.find_element(By.LINK_TEXT, label_name)
