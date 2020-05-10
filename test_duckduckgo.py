4689edab72adea05a7ab5b361e542c3cfd2cc331
DorDob <64862484+DorDob@users.noreply.github.com>
2020-05-10 14:12:24 +0200

    Update test_duckduckgo.py

--- test_duckduckgo.py
+++ test_duckduckgo.py
@@ -4,28 +4,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_searching_in_duckduckgo():
-    # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera
-
-    # ustawiana automatycznie przez bibliotekę webdriver-manager
+                                           # Uruchomienie przeglądarki Chrome. Ścieżka do chromedrivera        
+                                           # ustawiana automatycznie przez bibliotekę webdriver-manager                    
     browser = Chrome(executable_path=ChromeDriverManager().install())
-
-    # Otwarcie strony duckduckgo
+                                           # Otwarcie strony duckduckgo
     browser.get('https://duckduckgo.com/')
-    # Znalezienie paska wyszukiwania
+                                           # Znalezienie paska wyszukiwania
     search_form = browser.find_element(By.CSS_SELECTOR, '#search_form_input_homepage')
 
-    # Znalezienie guzika wyszukiwania (lupki)
+                                           # Znalezienie guzika wyszukiwania (lupki)
     search_button = browser.find_element_by_id('search_button_homepage')
 
-    # Asercje że elementy są widoczne dla użytkownika
+                                           # Asercje że elementy są widoczne dla użytkownika
     assert search_form.is_displayed()
-    assert search_button.is_displayed() is True  # nie trzeba pisa is True, bo inaczej jak bedzie False to i tak test się wywali
+    assert search_button.is_displayed() is True 
 
-    # Szukanie Vistula University
+                                           # Szukanie Vistula University
     search_form.send_keys('Vistula University')
     search_button.click()
 
-    # Sprawdzenie że akikolwiek wynik ma tytuł 'Vistula University in Warsaw'
+                                           # Sprawdzenie że akikolwiek wynik ma tytuł 'Vistula University in Warsaw'
     list = browser.find_elements(By.CSS_SELECTOR, '.result__title')
     list_of_titles = []
     for i in list:
@@ -33,7 +31,6 @@ def test_searching_in_duckduckgo():
     assert 'Vistula University in Warsaw' in list_of_titles
     assert list == 'Vistula University in Warsaw'
 
-
-    # Zamknięcie przeglądarki
+                                           # Zamknięcie przeglądarki
 
     browser.quit()
